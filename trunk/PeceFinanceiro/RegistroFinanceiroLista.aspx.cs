691d3ab2eb6393784d8b0711dad7b95c14a3f437
fgutiyama <fgutiyama@85083e56-1dae-11de-8297-c3254a27cc66>
2009-04-09 01:12:43 +0000

    Botoes adicionados à pagina de RegistroFinanceiroLista.aspx

--- trunk/PeceFinanceiro/RegistroFinanceiroLista.aspx.cs
+++ trunk/PeceFinanceiro/RegistroFinanceiroLista.aspx.cs
@@ -23,6 +23,8 @@ namespace PeceFinanceiro
 
             List<AlunoProjeto> listAlunoProjeto = new List<AlunoProjeto>();
 
+            GridViewListaRegistros.Columns.Clear();
+
             foreach (RegistroFinanceiro reg in listRegistros)
                 listAlunoProjeto.Add(reg.AlunoProjeto);
 
@@ -44,8 +46,55 @@ namespace PeceFinanceiro
             bfStatus.HeaderText = "Status";
             GridViewListaRegistros.Columns.Add(bfStatus);
 
+            CommandField cmdField = new CommandField();
+            cmdField.ButtonType = ButtonType.Image;
+            cmdField.DeleteImageUrl = "Icons/cross.png";
+            cmdField.EditImageUrl = "Icons/page_edit.png";
+            cmdField.SelectImageUrl = "Icons/money_add.png";
+            cmdField.ShowDeleteButton = true;
+            cmdField.ShowEditButton = true;
+            cmdField.ShowSelectButton = true;
+            cmdField.EditText = "Editar Registro";
+            cmdField.DeleteText = "Remover Regisro";
+            cmdField.SelectText = "Registrar Pagamentos";
+
+            GridViewListaRegistros.Columns.Add(cmdField);
+
             GridViewListaRegistros.DataBind();
+
+        }
+
+        protected void GridViewListaRegistros_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            List<AlunoProjeto> listAlunoProjeto = (List<AlunoProjeto>)GridViewListaRegistros.DataSource;
+            Int32 IdAlunoProjeto = listAlunoProjeto[e.NewEditIndex].Id;
+
+            Response.Redirect("RegistroFinanceiroNovoEditar.aspx?idRegistro=" + IdAlunoProjeto);
+        }
+
+        protected void GridViewListaRegistros_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            List<AlunoProjeto> listAlunoProjeto = (List<AlunoProjeto>)GridViewListaRegistros.DataSource;
+            Int32 IdAlunoProjeto = listAlunoProjeto[e.RowIndex].Id;
+
+            Response.Redirect("RegistroFinanceiroRemover.aspx?idRegistro=" + IdAlunoProjeto);
+        }
+
+        protected void GridViewListaRegistros_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "Select")
+            {
+                List<AlunoProjeto> listAlunoProjeto = (List<AlunoProjeto>)GridViewListaRegistros.DataSource;
+                Int32 IdAlunoProjeto = listAlunoProjeto[Int32.Parse(e.CommandArgument.ToString())].Id;
+
+                Response.Redirect("RegistroPagamentos.aspx?idRegistro=" + IdAlunoProjeto);
+            }
+        }
+
+        protected void GridViewListaRegistros_SelectedIndexChanged(object sender, EventArgs e)
+        {
 
         }
     }
 }
+
