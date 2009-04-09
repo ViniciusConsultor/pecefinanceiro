ef97a87017ec4eae44731fbf6b86175c7afd1113
fgutiyama <fgutiyama@85083e56-1dae-11de-8297-c3254a27cc66>
2009-04-09 06:05:41 +0000

    Editar Registro Financeiro almost working.

--- PeceFinanceiro/RegistroFinanceiroNovoEditar.aspx.cs
+++ PeceFinanceiro/RegistroFinanceiroNovoEditar.aspx.cs
@@ -17,7 +17,8 @@ namespace PeceFinanceiro
 {
     public partial class CadastroFinanceiro : System.Web.UI.Page
     {
-        CultureInfo _culture = new CultureInfo("pt-BR"); 
+        CultureInfo _culture = new CultureInfo("pt-BR");
+        RegistroFinanceiro registroFinanceiro = null;
         protected void Page_Load(object sender, EventArgs e)
         {
             BindJSEvents();
@@ -25,8 +26,26 @@ namespace PeceFinanceiro
             if (!IsPostBack)
             {
                 FillComboProjetos();
-                SelecionaProjeto();
+
+                int idRegistroSelecionado = -1;
+                if (!(Request.QueryString["idRegistro"] == null))
+                {
+                    idRegistroSelecionado = Int32.Parse(Request.QueryString["idRegistro"]);
+                    RegistroFinanceiroNegocio registroNegocio = new RegistroFinanceiroNegocio();
+                    registroFinanceiro = registroNegocio.ObterRegistroPorId(idRegistroSelecionado);
+
+                    SelecionaProjeto(registroFinanceiro.AlunoProjeto.Projeto.Codigo);
+                    SelecionaDdlAluno(registroFinanceiro.AlunoProjeto.Aluno.NumeroPece);
+                    FillAllFieldsToEdit(registroFinanceiro);
+                }
+                else
+                {
+                    SelecionaProjeto("");
+                }
+
             }
+
+            VerificaAlunoSelecionado();
 
             this.TextBoxValorComAjuste.ReadOnly = true;
             this.TextBoxValorParcela.ReadOnly = true;
@@ -34,6 +53,56 @@ namespace PeceFinanceiro
             PanelSucesso.Visible = false;
             PanelErro.Visible = false;
             ButtonEditarParcelas.Visible = false;
+        }
+
+        private void VerificaAlunoSelecionado()
+        {
+            if (DropDownListAlunos.Items.Count == 0)
+            {
+                ShowErrorMessage("Não é possível incluir registro pois não há aluno para o projeto selecionado.");
+                MudaStatusForm(false);
+            }
+            else
+                MudaStatusForm(true);
+        }
+
+        private void MudaStatusForm(bool enabled)
+        {
+            this.TextBoxAjusteValorFinal.Enabled = enabled;
+            this.TextBoxDataPrimeiraParcela.Enabled = enabled;
+            this.TextBoxDiaPagamento.Enabled = enabled;
+            this.TextBoxNumeroParcelas.Enabled = enabled;
+            this.TextBoxObservacoes.Enabled = enabled;
+            this.TextBoxValorComAjuste.Enabled = enabled;
+            this.TextBoxValorParcela.Enabled = enabled;
+            this.ButtonCadastrar.Enabled = enabled;
+        }
+
+        private void FillAllFieldsToEdit(RegistroFinanceiro registroFinanceiro)
+        {
+            this.TextBoxValorComAjuste.Text = registroFinanceiro.PrecoReajustado.ToString("#0.00").Replace('.', ',');
+            this.TextBoxNumeroParcelas.Text = Convert.ToString(registroFinanceiro.NumeroParcelas);
+            this.TextBoxDiaPagamento.Text = Convert.ToString(registroFinanceiro.DiaPagamento);
+            this.TextBoxDataPrimeiraParcela.Text = Convert.ToString(registroFinanceiro.DataVencimentoPrimeiraParcela);
+
+            this.TextBoxValorComAjuste.ReadOnly = false;
+            this.PlaceHolderValorComAjuste.Visible = false;
+            this.PlaceHolderValorParcela.Visible = false;
+            this.ButtonEditarParcelas.Visible = true;
+        }
+
+        private void SelecionaDdlAluno(int idAluno)
+        {
+            int i = 0;
+            foreach (ListItem item in this.DropDownListAlunos.Items)
+            {
+                if (Int32.Parse(item.Value) == idAluno)
+                {
+                    this.DropDownListAlunos.SelectedIndex = i;
+                    break;
+                }
+                i++;
+            }
         }
 
         private void BindJSEvents()
@@ -49,7 +118,7 @@ namespace PeceFinanceiro
         {
             ProjetoNegocio projetoNegocio = new ProjetoNegocio();
             List<Projeto> listProjeto = projetoNegocio.ObterTodosProjetos();
-
+            
             this.DropDownListProjetos.DataSource = listProjeto;
             this.DropDownListProjetos.DataTextField = "Nome";
             this.DropDownListProjetos.DataValueField = "Codigo";
@@ -66,18 +135,41 @@ namespace PeceFinanceiro
 
         protected void DropDownListProjetos_SelectedIndexChanged(object sender, EventArgs e)
         {
-            SelecionaProjeto();
+            SelecionaProjeto(String.Empty);
         }
 
-        private void SelecionaProjeto()
+        private void SelecionaProjeto(string idProjeto)
         {
             ProjetoNegocio projetoNegocio = new ProjetoNegocio();
-            Projeto projeto = projetoNegocio.ObterProjetoPorCodigo(this.DropDownListProjetos.SelectedValue);
+            Projeto projeto;
+            if (!idProjeto.Equals(String.Empty))
+            {
+                projeto = projetoNegocio.ObterProjetoPorCodigo(idProjeto);
+                ChangeDdlProjetosSelectedItem(idProjeto);
+            }
+            else
+                projeto = projetoNegocio.ObterProjetoPorCodigo(this.DropDownListProjetos.SelectedValue);
+
 
             this.HiddenFieldValorCurso.Value = Convert.ToString(projeto.Valor).Replace('.', ',');
             this.LabelValorFinal.Text = "R$ " + projeto.Valor.ToString("#0.00").Replace('.', ',');
 
             FillComboAlunos(projeto);
+            VerificaAlunoSelecionado();
+        }
+
+        private void ChangeDdlProjetosSelectedItem(string idProjeto)
+        {
+            int i = 0;
+            foreach (ListItem item in this.DropDownListProjetos.Items)
+            {
+                if (item.Value.Equals(idProjeto))
+                {
+                    this.DropDownListProjetos.SelectedIndex = i;
+                    break;
+                }
+                i++;
+            }
         }
 
         protected void DropDownListAlunos_SelectedIndexChanged(object sender, EventArgs e)
@@ -126,14 +218,19 @@ namespace PeceFinanceiro
             }
             else
             {
-                PanelErro.Visible = true;
-                MensagemErro.Text = errorMessage;
+                ShowErrorMessage(errorMessage);
             }
+        }
+
+        private void ShowErrorMessage(string errorMessage)
+        {
+            PanelErro.Visible = true;
+            MensagemErro.Text = errorMessage;
         }
 
         protected void ButtonCancelar_Click(object sender, EventArgs e)
         {
-            Response.Redirect("ListaCadastros.aspx");
+            Response.Redirect("RegistroFinanceiroLista.aspx");
         }
 
 
