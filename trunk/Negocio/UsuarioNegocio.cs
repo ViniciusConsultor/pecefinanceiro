0bcfdf6a99a4e5b2f596b9d1be95706a4f3e010a
leandrodvd <leandrodvd@85083e56-1dae-11de-8297-c3254a27cc66>
2009-04-12 17:31:21 +0000

    login

---
+++ trunk/Negocio/UsuarioNegocio.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Vsf.DAL;
+using Vsf.Negocio;
+using Vsf.Modelo;
+
+namespace Vsf.Negocio
+{
+    public class UsuarioNegocio
+    {
+        UsuarioDAO _usuariodao = new UsuarioDAO();
+
+        public Usuario ConsultarUsuario(string login)
+        {
+            return _usuariodao.ConsultarUsuarioPeloLogin(login);
+        }
+
+        public bool Autenticar(string login, string senha)
+        {
+            return _usuariodao.Autenticar(login,senha);
+        }
+    }
+}
---
+++ trunk/PeceFinanceiro/public/Login.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using Vsf.Modelo;
+using Vsf.Negocio;
+
+namespace PeceFinanceiro
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        Usuario _usuario = new Usuario();
+        UsuarioNegocio _usuarionegocio = new UsuarioNegocio();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            LoginFinanceiro.Focus();
+        }
+
+        protected void LoginFinanceiro_Authenticate(object sender, AuthenticateEventArgs e)
+        {
+            if (_usuarionegocio.Autenticar(LoginFinanceiro.UserName, LoginFinanceiro.Password))
+            {
+                _usuario = _usuarionegocio.ConsultarUsuario(LoginFinanceiro.UserName);
+               Session.Add("usuario",_usuario);
+               FormsAuthentication.RedirectFromLoginPage(LoginFinanceiro.UserName, true);
+            }
+        }
+    }
+}
